3b113cbd66b15f43f17565a62973f2b8db6e11e0
Jackson Chan <135435455+jacksonhk@users.noreply.github.com>
2023-12-30 15:26:21 +0800

    Update algotrade.py

--- algotrade.py
+++ algotrade.py
@@ -14,7 +14,7 @@ class AlgoEvent:
         self.arr_bbw = numpy.array([])
         self.bbw_len = 1*30 # length of bbw, only set to 30 as too high will yield no sequeeze, may change
         self.bbstd = 2
-        self.arr_close_dict = {} # key to the corresponding arr_close
+        self.arr_close_dict = {} # instrument to the corresponding arr_close
         self.allocationratio_per_trade = 0.3
         self.openOrder = {}
         self.netOrder = {}
@@ -22,7 +22,9 @@ class AlgoEvent:
 
     def start(self, mEvt):
         self.myinstrument = mEvt['subscribeList'][0]
+        self.no_instrument = len(mEvt['subscribeList'])
         self.evt = AlgoAPI_Backtest.AlgoEvtHandler(self, mEvt)
+        self.evt.update_portfolio_sl(sl=self.risk_limit_portfolio, resume_after=60*60*24*self.cooldown)
         self.evt.start()
 
 
@@ -30,33 +32,41 @@ class AlgoEvent:
         # set start time and arr_close(s) of all instruments in bd on the first call of this function
         if not self.start_time:
             self.start_time = bd[self.myinstrument]['timestamp']
-            for key in bd:
-                #self.evt.consoleLog(f"stock dict: {bd[key]}")
-                self.arr_close_dict[key] = numpy.array([])
+            for instrument in bd:
+                #self.evt.consoleLog(f"stock dict: {bd[instrument]}")
+                self.arr_close_dict[instrument] = numpy.array([])
+                self.arr_high_dict[instrument] = numpy.array([])
+                self.arr_low_dict[instrument] = numpy.array([])
         
         # check if it is decision time
         if bd[self.myinstrument]['timestamp'] >= self.lasttradetime + timedelta(hours=24):
             # update arr_close(s)
             self.lasttradetime = bd[self.myinstrument]['timestamp']
             #self.evt.consoleLog(f"wwwwwwwwwwwwwwwwwwwwwwwwwwwwwww")
-            for key in bd:
-                lastprice = bd[key]['lastPrice']
-                arr_close = self.arr_close_dict[key]
+            for instrument in bd:
+                lastprice = bd[instrument]['lastPrice']
+                highprice = bd[instrument]['highPrice']
+                lowprice = bd[instrument]['lowPrice']
+                arr_close = self.arr_close_dict[instrument]
                 #self.evt.consoleLog(f"arr close: {arr_close}")
                 
                 
-                self.arr_close_dict[key] = numpy.append(self.arr_close_dict[key], lastprice)
-            
+                self.arr_close_dict[instrument] = numpy.append(self.arr_close_dict[instrument], lastprice)
+                self.arr_high_dict[instrument] = numpy.append(self.arr_high_dict[instrument], highprice)
+                self.arr_low_dict[instrument] = numpy.append(self.arr_low_dict[instrument], lowprice)
+                
                 # keep the most recent observations for arr_close (record of close prices)
-                self.arr_close_dict[key] = self.arr_close_dict[key][-self.ma_len::]
+                self.arr_close_dict[instrument] = self.arr_close_dict[instrument][-self.ma_len::]
+                self.arr_high_dict[instrument] = self.arr_high_dict[instrument][-self.ma_len::]
+                self.arr_low_dict[instrument] = self.arr_low_dict[instrument][-self.ma_len::]
             
             # check if we have waited the initial peroid
             if bd[self.myinstrument]['timestamp'] <= self.start_time + timedelta(days = self.wait_time):
                 return
             
             # execute the trading strat for all instruments
-            for key in bd:
-                self.execute_strat(bd, key)
+            for instrument in bd:
+                self.execute_strat(bd, instrument)
             
             
     def on_marketdatafeed(self, md, ab):
@@ -76,13 +86,13 @@ class AlgoEvent:
     def find_sma(self, data, window_size):
         return data[-window_size::].sum()/window_size
 
-    # execute the trading strat for one instructment given the key and bd       
-    def execute_strat(self, bd, key):
+    # execute the trading strat for one instructment given the instrument and bd       
+    def execute_strat(self, bd, instrument):
         self.evt.consoleLog("---------------------------------")
         self.evt.consoleLog("Executing strat")
 
         # find sma, sd, 2 bbands, bbw, and lastprice
-        arr_close = self.arr_close_dict[key]
+        arr_close = self.arr_close_dict[instrument]
         sma = self.find_sma(arr_close, self.ma_len)
         sd = numpy.std(arr_close)
         upper_bband = sma + self.bbstd * sd
@@ -97,7 +107,7 @@ class AlgoEvent:
         #is_sequeeze = self.is_sequeeze(self.arr_bbw)
         
         # debug
-        self.evt.consoleLog(f"name of instrument: { bd[key]['instrument'] }")
+        self.evt.consoleLog(f"name of instrument: { bd[instrument]['instrument'] }")
         #self.evt.consoleLog(f"datetime: {bd[self.myinstrument]['timestamp']}")
         #self.evt.consoleLog(f"sma: {sma}")
         #self.evt.consoleLog(f"upper: {upper_bband}")
@@ -108,6 +118,7 @@ class AlgoEvent:
         if lastprice >= upper_bband:
             # caclulate the rsi
             rsi = talib.RSI(arr_close, timeperiod = self.rsi_len)
+            atr = talib.ATR(arr_close, timeperiod = )
             self.evt.consoleLog(f"rsi: {rsi[-1]}")
             # check for rsi
             if rsi[-1] > 70:
